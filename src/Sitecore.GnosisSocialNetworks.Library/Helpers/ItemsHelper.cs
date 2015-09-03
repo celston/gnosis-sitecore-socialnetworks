b4bf4ca3db2176756c88c2627db639dea9fbf10e
Chris Elston <celston@gmail.com>
2015-09-03 11:05:21 -0500

    First commit

---
+++ src/Sitecore.GnosisSocialNetworks.Library/Helpers/ItemsHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Sitecore.Data.Items;
+
+namespace Sitecore.GnosisSocialNetworks.Library.Helpers
+{
+    public class ItemsHelper
+    {
+        Lazy<Item> lazyRootItem;
+        
+        #region Singleton Setup
+
+        private readonly static Lazy<ItemsHelper> lazy = new Lazy<ItemsHelper>(() =>
+        {
+            return new ItemsHelper();
+        });
+
+        private ItemsHelper()
+        {
+            lazyRootItem = new Lazy<Item>(() =>
+            {
+                return Sitecore.Context.Database.GetItem(Sitecore.Context.Site.RootPath);
+            });
+        }
+
+        public static ItemsHelper Instance
+        {
+            get
+            {
+                return lazy.Value;
+            }
+        }
+
+        #endregion
+
+        public Item RootItem
+        {
+            get
+            {
+                return lazyRootItem.Value;
+            }
+        }
+    }
+}
---
+++ src/Sitecore.GnosisSocialNetworks/Areas/GnosisSocialNetworks/Models/Twitter/TwitterAppCardModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.GnosisSocialNetworks.Areas.GnosisSocialNetworks.Models.Twitter
+{
+    public class TwitterAppCardModel : TwitterCardModel
+    {
+        public override string CardType
+        {
+            get { return "app"; }
+        }
+
+        public bool ShowAppIphoneName
+        {
+            get { return !RenderingItemFieldIsNullOrWhitespace("Twitter Card App iPhone Name"); }
+        }
+
+        public bool ShowAppIphoneId
+        {
+            get { return !RenderingItemFieldIsNullOrWhitespace("Twitter Card App iPhone ID"); }
+        }
+
+        public bool ShowAppIphoneUrl
+        {
+            get { return !RenderingItemFieldIsNullOrWhitespace("Twitter Card App iPhone URL"); }
+        }
+
+        public bool ShowAppIpadName
+        {
+            get { return !RenderingItemFieldIsNullOrWhitespace("Twitter Card App iPad Name"); }
+        }
+
+        public bool ShowAppIpadId
+        {
+            get { return !RenderingItemFieldIsNullOrWhitespace("Twitter Card App iPad ID"); }
+        }
+
+        public bool ShowAppIpadUrl
+        {
+            get { return !RenderingItemFieldIsNullOrWhitespace("Twitter Card App iPad URL"); }
+        }
+
+        public bool ShowAppGooglePlayName
+        {
+            get { return !RenderingItemFieldIsNullOrWhitespace("Twitter Card App Google Play Name"); }
+        }
+
+        public bool ShowAppGooglePlayId
+        {
+            get { return !RenderingItemFieldIsNullOrWhitespace("Twitter Card App Google Play ID"); }
+        }
+
+        public bool ShowAppGooglePlayUrl
+        {
+            get { return !RenderingItemFieldIsNullOrWhitespace("Twitter Card App Google Play URL"); }
+        }
+    }
+}
